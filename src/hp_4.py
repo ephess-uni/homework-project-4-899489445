ac3568f1ac8e2871fbd3b6dbbb8f728ccafada50
899489445 <128246625+899489445@users.noreply.github.com>
2023-04-20 12:45:13 -0500

    completed

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,27 +8,62 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    new_dates = []
+    for date in old_dates:
+        datetime_obj = datetime.strptime(date, '%Y-%m-%d')
+        new_date = datetime_obj.strftime('%d %b %Y')
+        new_dates.append(new_date)
+    return new_dates
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinstance(start, str):
+        raise TypeError("Start date must be a string.")
+    if not isinstance(n, int):
+        raise TypeError("Number of days must be an integer.")
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    dates = [start_date + timedelta(days=i) for i in range(n)]
+    return dates
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    if not isinstance(start_date, str):
+        raise TypeError("start_date must be a string")
+    date_objects = date_range(start_date, len(values))
+    return list(zip(date_objects, values))
+
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    late_fees_by_patron = defaultdict(float)
+
+    with open(infile, 'r') as f:
+        reader = DictReader(f)
+        
+        for row in reader:
+            date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
+            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%Y')
+            
+            days_late = (date_returned - date_due).days
+            if days_late > 0:
+                late_fee = days_late * 0.25
+                
+                late_fees_by_patron[row['patron_id']] += late_fee
+
+    with open(outfile, 'w', newline='') as f:
+        writer = DictWriter(f, fieldnames=['patron_id', 'late_fees'])
+        writer.writeheader()
+        
+        for patron_id, late_fees in late_fees_by_patron.items():
+            writer.writerow({'patron_id': patron_id, 'late_fees': '{:.2f}'.format(late_fees)})
 
 
 # The following main selection block will only run when you choose
